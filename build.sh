fd9029f0f38b29a378f2807ea5263366cf843425
Andreas Opferkuch <andreas.opferkuch@gmail.com>
2020-05-23 22:54:43 +0200

    Use O3 optimization for emcc

--- build.sh
+++ build.sh
@@ -39,11 +39,15 @@ echo "======================"
 
 rm -rf build
 [ -d build ] || mkdir build > /dev/null 2>&1
+# On using O3 without breaking the binary:
+# https://github.com/emscripten-core/emscripten/issues/6882#issuecomment-406745898
+# (Results in ~20% better performance for 4K encoding, 5% for 512x512)
 time emcc \
   -s WASM=1 \
-  -O2 \
+  -O3 \
   -s MODULARIZE=1 \
   -s EXTRA_EXPORTED_RUNTIME_METHODS='[cwrap, getValue]' \
+  -s EXPORTED_FUNCTIONS='[_free, _malloc]' \
   -s ALLOW_MEMORY_GROWTH=1 \
   -I libwebp \
   -o ./build/wasm_webp.js \
